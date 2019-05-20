7e0fbdd513fe04a61c024edca0288be4db735652
Benedikt Lüken-Winkels <s4beluek@uni-trier.de>
2019-05-20 14:24:16 +0200

    init

---
+++ Assets/Scripts/Block/PreviewBlock.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PreviewBlock : MonoBehaviour
+{
+
+    public GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 playerpos = player.transform.position;
+        Vector3 posInGrid = playerpos + (player.transform.forward * 4);
+
+        posInGrid.x = (float)System.Math.Floor(posInGrid.x);
+        posInGrid.y = (float)System.Math.Floor(posInGrid.y);
+        posInGrid.z = (float)System.Math.Floor(posInGrid.z);
+
+       
+        transform.rotation = Quaternion.identity;
+        transform.position = posInGrid;
+
+        Collider[] colliders;
+        colliders = Physics.OverlapSphere(transform.position, 1f /* Radius */);
+        if (colliders.Length > 1) //Presuming the object you are testing also has a collider 0 otherwise
+        {
+            foreach (Collider c in colliders)
+            {
+                if(c.gameObject.transform.position == transform.position && c.gameObject.GetComponent<PreviewBlock>() != this)
+                {
+                    GameObject selectedGo = c.gameObject;
+
+                    Block selectedBlock = selectedGo.GetComponent<Block>();
+
+                    // Delete Block
+                    if (Input.GetMouseButtonDown(1))
+                    {
+                        Destroy(selectedGo);
+                    }
+                    // Change Texture
+                    else if (Input.GetKeyDown(KeyCode.R))
+                    {
+                        selectedBlock.nextTexture();
+                    }
+                }
+            }
+
+
+
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Vector3 m_movement;
+    [SerializeField] float speedAmp;
+
+    float horizontalSpeed = 2.0f;
+    float verticalSpeed = 2.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vertical Movement
+        if(Input.GetKey(KeyCode.Space))
+        { 
+            m_movement = Vector3.up;
+
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            m_movement = Vector3.down;
+        }
+        // Horizontal Movement
+        else
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            m_movement.Set(horizontal, 0f, vertical);
+            m_movement.Normalize();
+        }
+
+        // Camera cotrolls
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(-v, h, 0f);
+
+        // Prevent z-axis rotation
+        Quaternion q = transform.rotation;
+        q.eulerAngles = new Vector3(q.eulerAngles.x, q.eulerAngles.y, 0);
+        transform.rotation = q;
+
+
+
+
+
+
+
+
+        transform.Translate(m_movement * speedAmp * Time.deltaTime);
+    }
+
+
+
+}
